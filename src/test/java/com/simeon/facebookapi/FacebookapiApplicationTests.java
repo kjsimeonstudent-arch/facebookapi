6ce7d22732e81f561a1d3a0d6cd8c114e9aca026
Kenzhin <kjsimeon.student@ua.edu.ph>
2025-11-07 09:11:15 +0800

    updated pip file

---
+++ src/test/java/com/simeon/facebookapi/FacebookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simeon.facebookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
